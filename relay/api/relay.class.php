9eaf826abe54a612b4546c7bda520d8b26d8e22a
Simon Skrødal <simon.skrodal@gmail.com>
2016-07-28 17:46:07 +0930

    Update version

--- relay/api/relay.class.php
+++ relay/api/relay.class.php
@@ -26,7 +26,7 @@
 		#
 		// /service/ endpoint - not sure if needed...
 		public function getServiceVersion() {
-			return $this->relaySQLConnection->query("SELECT * FROM tblVersion")[0];
+			return $this->relaySQLConnection->query("SELECT versValue FROM tblVersion")[0];
 		}
 
 
